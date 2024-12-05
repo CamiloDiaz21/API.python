464859cd838a18e3458f3c782d50f3857df15364
kamilodiaz0521@gmail.com <kamilodiaz0521@gmail.com>
2024-12-05 14:18:41 -0500

    Inicio de la API en leguaje python

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .serializer import ProgrammerSerializer
+from .models import programmer
+
+class ProgrammerViewSet(viewsets.ModelViewSet): 
+    queryset = programmer.objects.all()
+    serializer_class = ProgrammerSerializer
+
+# Create your views here.
